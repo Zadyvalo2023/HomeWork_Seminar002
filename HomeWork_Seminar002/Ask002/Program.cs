0ba6567be732d25bca30e222154931cf9546f7c8
Evgeny Smozhevsky <129326244+Zadyvalo2023@users.noreply.github.com>
2023-06-17 01:51:09 +0300

    Add files via upload

---
+++ HomeWork_Seminar002/Ask002/Program.cs
@@ -0,0 +1,68 @@
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num < 100) {
+      Console.WriteLine("Третьей цифры нет");
+    }
+    else {
+      int thirdDigit = (num / 100) % 10;
+      Console.WriteLine("Третья цифра: " + thirdDigit);
+    }
+  }
+}
+
+
+
+
+/*
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+        
+        if (input.Length >= 3) {
+            Console.WriteLine("Третья цифра: " + input[2]);
+        } else {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+    }
+}*/
+/*
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int thirdDigit = GetThirdDigit(num);
+
+        if (thirdDigit != -1) {
+            Console.WriteLine($"Третья цифра числа {num} равна {thirdDigit}");
+        } else {
+            Console.WriteLine($"У числа {num} нет третьей цифры");
+        }
+    }
+
+    static int GetThirdDigit(int num) {
+        if (num >= 100 && num <= 999) {
+            return (num / 10) % 10;
+        } else {
+            return -1;
+        }
+    }
+}
+*/
---
+++ HomeWork_Seminar002/Ask003/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу которая принимает на вход цифру ,
+//   обозначающую день недели , и проверяет , являеться ли этот день выходным.
+
+
+
+System.Console.Write("Введите день недели: ");
+int WeekDay = Convert.ToInt32(Console.ReadLine());
+string[] message = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" , "Воскресенье"};
+if (WeekDay > 0 && WeekDay < 8)
+{
+    System.Console.WriteLine(message[WeekDay - 1]);
+    if (WeekDay > 5 && WeekDay < 8)
+    {
+        System.Console.WriteLine("Это выходной день!");
+    }
+}
+else
+{
+    System.Console.WriteLine("Такого дня не существует!");
+}
+
+/*
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите день недели (1-7): ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        if (dayOfWeek == 6 || dayOfWeek == 7) {
+            Console.WriteLine("Это выходной день!");
+        } else {
+            Console.WriteLine("Это рабочий день!");
+        }
+
+        Console.ReadLine();
+    }
+}*/
